9d04457bfeb1c977520456708e7973dfead27511
salasrew <43160416+salasrew@users.noreply.github.com>
2022-07-28 10:20:49 +0800

    Create leetcode 2351.py

---
+++ leetcode 2351.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        dict_ = {}
+        for i in s:
+            if i not in dict_:
+                dict_[i] = 0
+            else:
+                return i
